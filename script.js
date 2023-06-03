d515977dc15b1ced24622197f89b8490976bc032
HossainRafi <hossainrafi71@gmail.com>
2023-06-03 22:06:06 +0600

    alert added

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ const decreaseNumber = (incdec, itemprice) => {
   var itemprice = document.getElementById(itemprice);
   if (itemval.value <= 0) {
     itemval.value = 0;
-    alert("Negative quantity not allowed");
+    Swal.fire({
+      icon: "warning",
+      title: "Negative quantity not allowed!",
+    });
   } else {
-      itemval.value = parseInt(itemval.value) - 1;
-      itemval.style.background = "white";
-      itemval.style.color = "black";
+    itemval.value = parseInt(itemval.value) - 1;
+    itemval.style.background = "white";
+    itemval.style.color = "black";
+    //   itemprice.innerHTML = parseInt(itemprice.innerHTML) - 15;
   }
 };
 
@@ -16,11 +20,14 @@ const increaseNumber = (incdec, itemprice) => {
   var itemprice = document.getElementById(itemprice);
   if (itemval.value >= 5) {
     itemval.value = 5;
-    alert("5 Products in stock");
+    Swal.fire({
+      icon: "warning",
+      title: "Can't purchase more than 5 products!",
+    });
     itemval.style.background = "red";
     itemval.style.color = "white";
   } else {
-      itemval.value = parseInt(itemval.value) + 1;
-      
+    itemval.value = parseInt(itemval.value) + 1;
+    // itemprice.innerHTML = parseInt(itemprice.innerHTML) + 15;
   }
 };
