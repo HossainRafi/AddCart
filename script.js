de74f2f371ac829f5aaac97f4c4d0c73cf051392
HossainRafi <hossainrafi71@gmail.com>
2023-06-03 20:39:14 +0600

    increase number

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const decreaseNumber = () => {
-  let itemval = document.getElementById("textbox");
+  var itemval = document.getElementById("textbox");
   if (itemval.value <= 0) {
     itemval.value = 0;
     alert("Negative quantity not allowed");
   } else {
-    itemval.value = parseInt(itemval.value) - 1;
+      itemval.value = parseInt(itemval.value) - 1;
+      itemval.style.background = "white";
+      itemval.style.color = "black";
+  }
+};
+
+const increaseNumber = () => {
+  var itemval = document.getElementById("textbox");
+  if (itemval.value >= 5) {
+    itemval.value = 5;
+      alert("5 Products in stock");
+      itemval.style.background = "red"
+      itemval.style.color="white"
+  } else {
+    itemval.value = parseInt(itemval.value) + 1;
   }
 };
