7f11d4e5dc3e83f9a17579729e2c9e41c40c7a58
HossainRafi <hossainrafi71@gmail.com>
2023-06-11 09:32:41 +0600

    price update

--- script.js
+++ script.js
@@ -1,33 +1,59 @@
-const decreaseNumber = (incdec, itemprice) => {
-  var itemval = document.getElementById(incdec);
-  var itemprice = document.getElementById(itemprice);
-  if (itemval.value <= 0) {
-    itemval.value = 0;
+const decreaseNumber = (price, quantity, total) => {
+  var getPrice = document.getElementById(price).textContent;
+  console.log(getPrice, "price");
+  var getTotal = document.getElementById(total);
+  console.log(getTotal, "Total");
+  var getQuantity = document.getElementById(quantity);
+  console.log(getQuantity, "quantity");
+  var productTotal = document.getElementById("product_total_amt");
+
+  if (getQuantity.value <= 0) {
     Swal.fire({
       icon: "warning",
-      title: "Negative quantity not allowed!",
+      title: "Minimum quantity will be 1",
     });
+    getQuantity.style.background = "red";
+    getQuantity.style.color = "white";
   } else {
-    itemval.value = parseInt(itemval.value) - 1;
-    itemval.style.background = "white";
-    itemval.style.color = "black";
-    //   itemprice.innerHTML = parseInt(itemprice.innerHTML) - 15;
+    getQuantity.style.background = "white";
+    getQuantity.style.color = "red";
+    getQuantity.value = parseInt(getQuantity.value) - 1;
   }
+
+  getTotal.innerText = parseInt(getPrice) * parseInt(getQuantity.value);
+  all_product_price =
+    parseInt(document.getElementById("total").textContent) +
+    parseInt(document.getElementById("total2").textContent);
+  productTotal.innerText = all_product_price;
+  document.getElementById("total_cart_amt").innerText = all_product_price + 50;
 };
 
-const increaseNumber = (incdec, itemprice) => {
-  var itemval = document.getElementById(incdec);
-  var itemprice = document.getElementById(itemprice);
-  if (itemval.value >= 5) {
-    itemval.value = 5;
+const increaseNumber = (price, quantity, total) => {
+  var getPrice = document.getElementById(price).textContent;
+  console.log(getPrice,'price');
+  var getTotal = document.getElementById(total);
+  console.log(getTotal,'Total');
+  var getQuantity = document.getElementById(quantity);
+  console.log(getQuantity,'quantity');
+  var productTotal = document.getElementById("product_total_amt");
+
+  if (getQuantity.value >= 5) {
     Swal.fire({
       icon: "warning",
       title: "Can't purchase more than 5 products!",
     });
-    itemval.style.background = "red";
-    itemval.style.color = "white";
+    getQuantity.style.background = "red";
+    getQuantity.style.color = "white";
   } else {
-    itemval.value = parseInt(itemval.value) + 1;
-    // itemprice.innerHTML = parseInt(itemprice.innerHTML) + 15;
+    getQuantity.style.background = "white";
+    getQuantity.style.color = "red";
+    getQuantity.value = parseInt(getQuantity.value) + 1;
   }
+
+  getTotal.innerText = parseInt(getPrice) * parseInt(getQuantity.value);
+  all_product_price =
+    parseInt(document.getElementById("total").textContent) +
+    parseInt(document.getElementById("total2").textContent);
+  productTotal.innerText = all_product_price;
+  document.getElementById("total_cart_amt").innerText = all_product_price + 50;
 };
